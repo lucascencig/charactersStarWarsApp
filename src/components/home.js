f5f6a235c912ec60481b820c8751b7582377911f
lucascencig <cenciglucas@gmail.com>
2022-09-21 22:47:12 -0300

    componente home mejorado sin errores

--- src/components/home.js
+++ src/components/home.js
@@ -1,38 +1,41 @@
 import React from 'react'
 import '../styles/bootstrap.min.css'
-
+import '../styles/Home.css'
 const Home = () => {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid bg-">
 
-      <h1 className="text-center bg-primary text-white">Star Wars Character App</h1>
-      <p className='text-center bg-primary text-white'>Find your favourite character from <span className='bg-danger'>Star Wars</span> and know all their details.</p>
+      <h1 className="bg-primary text-center text-white titulo">Star Wars Character App</h1>
+      <p className='text-center bg-success text-white'>Find your favourite character from <span className='bg-danger'>Star Wars</span> and know all their details.</p>
 
+      {/* ACA HAY QUE HACER EL MAPEO DE LOS PERSONAJES */}
       <div>
         <ul>
-          <td className='table-primary'>Nombre: { }</td>
+          <li className='table-primary'>Nombre: { }</li>
         </ul>
       </div>
 
 
       <aside className='container Primary'>
-        <table class="table">
+        <table className="table">
           <thead>
-            <td>Name:</td>
-            <td>Height: </td>
-            <td>Mass: </td>
-            <td>Hair Color:</td>
-            <td>Skin Color: { }</td>
-            <td>Eye Color: { }</td>
-            <td>Birth Year: { }</td>
-            <td>Gender: { }</td>
-            <td>Films: { }</td>
-            <td>Species: { }</td>
+            <tr>
+              <th>Name</th>
+              <th>Height</th>
+              <th>Mass</th>
+              <th>Hair Color</th>
+              <th>Skin Color</th>
+              <th>Eye Color</th>
+              <th>Birth Year</th>
+              <th>Gender</th>
+              <th>Films</th>
+              <th>Species</th>
+            </tr>
           </thead>
           <tbody>
             <tr>
-              {/* ACA VA EL CODIGO JS PARA LOS DETALLES DEL PERSONAJE */}
-              <th scope="row">LUKE</th>
+              {/* ACA VA EL CODIGO JS PARA LOS DETALLES DEL PERSONAJE con las {} */}
+              <td scope="row">LUKE</td>
               <td>70KG </td>
               <td>75KG </td>
               <td>BLACK</td>
